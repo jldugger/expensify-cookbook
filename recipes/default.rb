34910dfaea7e8fb085620c6784ca07613d5835f2
Justin Dugger <jldugger@gmail.com>
2017-08-14 20:11:10 -0700

    manage expensify user by default

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,25 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+user 'expensify' do
+  comment 'a simple user'
+  manage_home true
+end
+
+directory '/home/expensify/.ssh/'
+
+cookbook_file '/home/expensify/.ssh/authorized_keys' do
+  mode '0600'
+  owner 'expensify'
+  source 'expensify-keys'
+end
+
+group 'sysadmin' do
+  members 'expensify'
+end
+
+node.default['sudo']['passwordless'] = true
+node.default['authorization']['sudo']['agent_forwarding'] = true
+
+include_recipe 'sudo'
