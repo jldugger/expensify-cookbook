33e8dc0ba0e122a29399a09bb51b23bd2be1c35f
Justin Dugger <jldugger@gmail.com>
2017-08-14 20:17:01 -0700

    fix metadata foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,22 +1,14 @@
 name 'expensify'
 maintainer 'Justin Dugger'
 maintainer_email 'jldugger@gmail.com'
-license 'All Rights Reserved'
+license 'GPL-2.0'
 description 'Installs/Configures expensify'
 long_description 'Installs/Configures expensify'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+supports 'ubuntu'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/expensify/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/expensify'
+issues_url 'https://github.com/jldugger/expensify-cookbook/issues'
+source_url 'https://github.com/jldugger/expensify-cookbook'
 
 depends 'sudo'
