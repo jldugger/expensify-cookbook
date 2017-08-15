414b61ea220ef6c11aafd3dda7e37544741200c4
Justin Dugger <jldugger@gmail.com>
2017-08-14 19:19:52 -0700

    generate recipes for deploying app 'a' and 'b'

---
+++ recipes/app_a.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: expensify
+# Recipe:: app_a
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
---
+++ recipes/app_b.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: expensify
+# Recipe:: app_b
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
