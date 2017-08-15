31c30de43e29a4950b3b67f6553017fa06b82c7e
Justin Dugger <jldugger@gmail.com>
2017-08-15 12:38:15 -0700

    fix rake

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,5 +5,5 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 describe user('expensify') do
-    it { should exist }
+  it { should exist }
 end
