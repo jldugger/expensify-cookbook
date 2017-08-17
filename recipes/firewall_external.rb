6cf1246cd6c85bd2aeea019cef318e11b3b363a4
Justin Dugger <jldugger@gmail.com>
2017-08-16 22:30:28 -0700

    fix nagios by allowing ping ^_^

--- recipes/firewall_external.rb
+++ recipes/firewall_external.rb
@@ -5,6 +5,7 @@
 # Copyright:: 2017, Justin Dugger, All Rights Reserved.
 
 node.default['firewall']['allow_ssh'] = true
+node.default['firewall']['allow_icmp'] = true
 node.default['firewall']['ubuntu_iptables'] = true
 include_recipe 'firewall::default'
 
