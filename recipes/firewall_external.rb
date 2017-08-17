804b46894ec8b5b4ffdd921fe82d92ebf84c3430
Justin Dugger <jldugger@gmail.com>
2017-08-16 17:12:00 -0700

    configure bastion host firewall rules

--- recipes/firewall_external.rb
+++ recipes/firewall_external.rb
@@ -3,3 +3,13 @@
 # Recipe:: firewall_external
 #
 # Copyright:: 2017, Justin Dugger, All Rights Reserved.
+
+node.default['firewall']['allow_ssh'] = true
+node.default['firewall']['ubuntu_iptables'] = true
+include_recipe 'firewall::default'
+
+firewall_rule 'http' do
+  port     80
+  protocol :tcp
+  command  :allow
+end
